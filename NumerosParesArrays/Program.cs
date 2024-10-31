d8be7e81d9833c309d0d2dffab01ddd1daae301e
Gabriela <gabriela.priaro@gmail.com>
2024-10-31 15:53:38 +0000

    Implementaçao para mostrar numeros pares entre os digitados pelo usuário e a quantidade deles.

--- NumerosParesArrays/Program.cs
+++ NumerosParesArrays/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace NumerosParesArrays
+﻿//Utilizando array:
+//Problema "numeros_pares"
+//Faça um programa que leia N números inteiros e armazene-os em um vetor. Em seguida, mostre na
+//tela todos os números pares, e também a quantidade de números pares. 
+
+namespace NumerosParesArrays
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n, contPares;
+
+            contPares = 0;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite um numero: ");
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("NUMEROS PARES: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                if (vet[i] % 2 == 0)
+                {
+                    contPares++;
+                    Console.Write(vet[i] + " ");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write($"QUANTIDADE DE PARES = {contPares}");
         }
     }
 }
